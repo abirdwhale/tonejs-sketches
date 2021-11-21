8aed0fab0124fe15c7d2a5c08ddeff331e865fe9
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-21 12:40:00 +0000

    making it cross-browser compatible

--- tonejs-template/js/app.js
+++ tonejs-template/js/app.js
@@ -16,10 +16,16 @@ function song(time){
   console.log(time);
 }
 
-document.getElementById("play-button").addEventListener("click", function () {
+/* document.getElementById("play-button").addEventListener("click", function () {
   if (Tone.Transport.state !== "started") {
     Tone.Transport.start();
   } else {
     Tone.Transport.stop();
   }
-});
+}); */
+
+//attach a click listener to a play button
+document.querySelector('play-button')?.addEventListener('click', async () => {
+	await Tone.start()
+	console.log('audio is ready')
+})
