723158f14e8207b08a53217550a0d3a4efa72831
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-22 21:03:11 +0000

    removed p5.js from sketch-kick-cymbal

--- sketch-kick-cymbal/js/app.js
+++ sketch-kick-cymbal/js/app.js
@@ -2,40 +2,37 @@ let loopBeat;
 let bassSynth, cymbalSynth;
 let counter;
 
-function setup() {
-  counter = 0;
+counter = 0;
 
-  bassSynth = new Tone.MembraneSynth().toDestination();
-  cymbalSynth = new Tone.MetalSynth({
-    "frequency": 250,
-    "envelope": {
-      "attack": 0.001,
-      "decay": 0.1,
-      "release": 0.01,
-    },
-    "harmonicity": 3.1,
-    "modulationIndex": 16,
-    "resonance": 4000,
-    "octaves": 0.5,
-  }).toDestination();
+bassSynth = new Tone.MembraneSynth().toDestination();
+cymbalSynth = new Tone.MetalSynth({
+  "frequency": 250,
+  "envelope": {
+    "attack": 0.001,
+    "decay": 0.1,
+    "release": 0.01,
+  },
+  "harmonicity": 3.1,
+  "modulationIndex": 16,
+  "resonance": 4000,
+  "octaves": 0.5,
+}).toDestination();
 
-  loopBeat = new Tone.Loop(song, "16n");
+loopBeat = new Tone.Loop(song, "16n");
 
-  Tone.Transport.bpm.value = 140;
-  Tone.Transport.start();
-  loopBeat.start(0);
-}
+Tone.Transport.bpm.value = 140;
+loopBeat.start(0);
 
 function song(time) {
   if (counter % 4 === 0) {
     bassSynth.triggerAttackRelease("c1", "8n", time, 1);
   }
-    
+
   if (counter % 4 !== 1) {
     if (counter === 3 || counter === 12) {
-        cymbalSynth.envelope.decay = 0.5;
+      cymbalSynth.envelope.decay = 0.5;
     } else {
-        cymbalSynth.envelope.decay = 0.01;
+      cymbalSynth.envelope.decay = 0.01;
     }
     cymbalSynth.triggerAttackRelease("c1", "32n", time, 0.3);
   }
@@ -50,10 +47,3 @@ document.getElementById("play-button").addEventListener("click", function () {
     Tone.Transport.stop();
   }
 });
-
-//below does not work and i don't know why...
-//attach a click listener to a play button
-/* document.querySelector('button')?.addEventListener('click', async () => {
-	await Tone.start()
-	console.log('audio is ready')
-}) */
