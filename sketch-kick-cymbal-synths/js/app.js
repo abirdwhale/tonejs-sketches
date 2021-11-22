c1108c03e8171bb3248bb3689e06ea0f7d0013bb
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-22 21:06:54 +0000

    removed p5.js from sketch-kick-cymbal-synths

--- sketch-kick-cymbal-synths/js/app.js
+++ sketch-kick-cymbal-synths/js/app.js
@@ -5,75 +5,72 @@ let amSynth;
 let fmSymth;
 let pluckSynth;
 
-function setup() {
-  counter = 0;
+counter = 0;
 
-  pluckSynth = new Tone.PluckSynth().toDestination();
+pluckSynth = new Tone.PluckSynth().toDestination();
 
-  amSynth = new Tone.AMSynth({
-    harmonicity: 1.04, //3/1
-    detune: 0,
-    oscillator: {
-      type: "sine", //cariier signal
-    },
-    envelope: {
-      attack: 0.0001,
-      decay: 0.01,
-      sustain: 1,
-      release: 0.5,
-    },
-    modulation: {
-      type: "square", //modulation signal
-    },
-    modulationEnvelope: {
-      attack: 0.5,
-      decay: 0,
-      sustain: 1,
-      release: 0.5,
-    },
-  }).toDestination();
+amSynth = new Tone.AMSynth({
+  harmonicity: 1.04, //3/1
+  detune: 0,
+  oscillator: {
+    type: "sine", //cariier signal
+  },
+  envelope: {
+    attack: 0.0001,
+    decay: 0.01,
+    sustain: 1,
+    release: 0.5,
+  },
+  modulation: {
+    type: "square", //modulation signal
+  },
+  modulationEnvelope: {
+    attack: 0.5,
+    decay: 0,
+    sustain: 1,
+    release: 0.5,
+  },
+}).toDestination();
 
-  fmSynth = new Tone.FMSynth({
-    harmonicity: 1.04, // modulation synth / carrier synth
-    modulationIndex: 10,
-    detune: 0,
-    oscillator: {
-      type: "sine",
-    },
-    envelope: {
-      attack: 0.01,
-      decay: 0.01,
-      sustain: 1,
-      release: 0.5,
-    },
-    modulation: {
-      type: "square",
-    },
-    modulationEnvelope: {
-      attack: 0.5,
-      decay: 0,
-      sustain: 1,
-      release: 0.5,
-    },
-  }).toDestination();
+fmSynth = new Tone.FMSynth({
+  harmonicity: 1.04, // modulation synth / carrier synth
+  modulationIndex: 10,
+  detune: 0,
+  oscillator: {
+    type: "sine",
+  },
+  envelope: {
+    attack: 0.01,
+    decay: 0.01,
+    sustain: 1,
+    release: 0.5,
+  },
+  modulation: {
+    type: "square",
+  },
+  modulationEnvelope: {
+    attack: 0.5,
+    decay: 0,
+    sustain: 1,
+    release: 0.5,
+  },
+}).toDestination();
 
-  bassSynth = new Tone.MembraneSynth().toDestination();
-  cymbalSynth = new Tone.MetalSynth({
-    frequency: 250,
-    envelope: {
-      attack: 0.001,
-      decay: 0.1,
-      release: 0.01,
-    },
-    harmonicity: 3.1,
-    modulationIndex: 16,
-    resonance: 4000,
-    octaves: 0.5,
-  }).toDestination();
+bassSynth = new Tone.MembraneSynth().toDestination();
+cymbalSynth = new Tone.MetalSynth({
+  frequency: 250,
+  envelope: {
+    attack: 0.001,
+    decay: 0.1,
+    release: 0.01,
+  },
+  harmonicity: 3.1,
+  modulationIndex: 16,
+  resonance: 4000,
+  octaves: 0.5,
+}).toDestination();
 
-  loopBeat = new Tone.Loop(song, "16n").start(0);;
-  Tone.Transport.start().bpm.value = 140;
-}
+loopBeat = new Tone.Loop(song, "16n").start(0);;
 
 function song(time) {
   if (counter % 4 === 0) {
@@ -118,7 +115,7 @@ function song(time) {
 
 document.getElementById("play-button").addEventListener("click", function () {
   if (Tone.Transport.state !== "started") {
-    Tone.Transport.start();
+    Tone.Transport.start().bpm.value = 140;
   } else {
     Tone.Transport.stop();
   }
