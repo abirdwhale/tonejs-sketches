b701b59a73785772b0e067fe02ee721cb5db2e48
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-11-26 17:06:26 +0000

    longer arpeggios

--- arpeggiator/js/app.js
+++ arpeggiator/js/app.js
@@ -3,15 +3,16 @@
 console.clear();
 
 const $inputs = document.querySelectorAll('input'),
-      chords = [
-        'A4 C5 E5', 'F4 A4 C5', 'G4 B4 D5',
-        'D4 F4 A4', 'E4 G4 B4' 
-      ].map(strong => strong.split(' '));
+  chords = [
+    'A0 C1 E1', 'F0 A0 C1', 'G0 B0 D1',
+    'D0 F0 A0', 'E0 G0 B0'
+  ].map(formatChords);
+console.log(chords);
 let chordIdx = 0,
-    step = 0;
+  step = 0;
 
 const synth = new Tone.Synth(),
-      gain = new Tone.Gain(0.2);
+  gain = new Tone.Gain(0.2);
 synth.oscillator.type = 'sine';
 gain.toDestination();
 synth.connect(gain);
@@ -27,7 +28,6 @@ Array.from($inputs).forEach($input => {
 function handleChord(valueString) {
   chordIdx = parseInt(valueString) - 1;
   console.log(chordIdx);
-  
 }
 
 
@@ -36,12 +36,27 @@ Tone.Transport.start();
 
 function onRepeat(time) {
   let chord = chords[chordIdx],
-      note = chord[step % 3];
-      
-      synth.triggerAttackRelease(note, '8n', time);
-      step++
+    note = chord[step % chord.length];
+
+  synth.triggerAttackRelease(note, '8n', time);
+  step++
 }
 
+// DOWN THE LINE THIS WILL MAKE THINGS EASIER
+function formatChords(chordString) {
+  let chord = chordString.split(' ');
+  let arr = [];
+  for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < chord.length; j++) {
+      let noteOct = chord[j].split(''),
+        note = noteOct[0];
+      let oct = (noteOct[1] === '0') ? i + 4 : i + 5;
+      note += oct;
+      arr.push(note);
+    }
+  }
+  return arr;
+}
 
 document.getElementById("play-button").addEventListener("click", function () {
   if (Tone.Transport.state !== "started") {
